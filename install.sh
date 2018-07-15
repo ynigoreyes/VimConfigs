2df10bb68d7d3601b9b012b3a4ecae7717cbac25
Miggy Reyes <ynigoreyes@gmail.com>
2018-07-15 07:34:24 -0500

    adding global js config

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 echo "Copying current .vimrc to root directory"
 cp .vimrc ~/.vimrc
+cp .tern-config ~/.tern-config
 
 echo "You must have python 3 installed to run some of the plugins"
 echo "Don't forget to run :PlugInstall!!!"
